11b2367f87d9b474ef6b7b436f5072a684bb94bf
Scott Chow <tehchowster@gmail.com>
2022-10-29 16:08:59 -0700

    Add Account Endpoint & Riot Env Var

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,12 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
 import { exampleRouter } from "./example";
+import { accountRouter } from "./getAccount";
 
 export const appRouter = router({
   example: exampleRouter,
   auth: authRouter,
+  getAccount: accountRouter,
 });
 
 // export type definition of API
